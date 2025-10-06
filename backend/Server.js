f0c7a506ccd9a9a069c54267dc3a81760951bc3f
LUCA-hub10 <aladin.luca@gmail.com>
2025-10-06 12:42:48 +0100

    Initial commit - backend et frontend sans secrets

--- backend/Server.js
+++ backend/Server.js
@@ -1,3 +1,15 @@
+require('dotenv').config();  // Au tout début du fichier
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log("MongoDB connecté"))
+.catch(err => console.log(err));
+
+
+/*
 // Charger les variables d'environnement
 require("dotenv").config();
 
@@ -25,3 +37,4 @@ app.use("/api/products", productRoutes);
 // Port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Serveur lancé sur le port ${PORT}`));
+*/
